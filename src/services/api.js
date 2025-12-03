5204e070fbee1b95c0bf6a1cf8dee30d5afb39a1
Anitha250792 <ntanithasaravanan@gmail.com>
2025-12-03 19:04:31 +0530

    Fix API base URL for Render

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,15 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000/api/",
+  baseURL: "https://cloud-hrms-1.onrender.com/api/",  // LIVE BACKEND
+});
+
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem("access");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default API;
